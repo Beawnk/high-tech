768d55f8113f9d39b86e96992e8f953bc3106adb
Bianca <bsineleck@gmail.com>
2024-12-06 02:25:21 -0300

    cart style done

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,26 +1,33 @@
 <template>
     <div class="cart-modal modal">
         <div class="modal-container" ref="modalRef">
-            <h2>Carrinho</h2>
-            <div class="cart-items" v-if="uniqueCartItems.length > 0">
-                <div class="cart-item" v-for="(item, index) in uniqueCartItems" :key="item.id">
-                    <div class="cart-item-img">
-                        <img :src="item.img" :alt="item.name" />
+            <div class="cart-content">
+                <h2>Carrinho</h2>
+                <div class="cart-items" v-if="uniqueCartItems.length > 0">
+                    <div class="cart-item" v-for="(item, index) in uniqueCartItems" :key="item.id">
+                        <div class="cart-wrap">
+                            <div class="cart-item-img">
+                            <img :src="item.img" :alt="item.name" />
+                            </div>
+                            <div class="cart-item-info">
+                                <h3>{{ item.name }}</h3>
+                                <p>{{ formatCurrency(item.price) }}</p>
+                            </div>
+                        </div>
+                        <div class="cart-item-buttons">
+                            <div class="cart-item-quantity">
+                                <button class="btn btn-quantity remove small light" @click="cart.removeProduct(index)"></button>
+                                <span>{{ itemQuantity(item.id) }}</span>
+                                <button class="btn btn-quantity add small light" @click="cart.addProduct(item)"></button>
+                            </div>
+                            <button class="btn btn-remove-all small" @click="cart.removeWholeProduct(item.id)"></button>
+                        </div>
+
                     </div>
-                    <div class="cart-item-info">
-                        <h3>{{ item.name }}</h3>
-                        <p>{{ formatCurrency(item.price) }}</p>
-                    </div>
-                    <div class="cart-item-quantity">
-                        <button class="btn btn-remove" @click="cart.removeProduct(index)">-</button>
-                        <span>{{ itemQuantity(item.id) }}</span>
-                        <button class="btn btn-add" @click="cart.addProduct(item)">+</button>
-                    </div>
-                    <button class="btn btn-remove-all" @click="cart.removeWholeProduct(item.id)">Remove</button>
                 </div>
-            </div>
-            <div class="no-items" v-else>
-                <h1>Seu carrinho está vazio</h1>
+                <div class="no-items" v-else>
+                    <h1>Seu carrinho está vazio</h1>
+                </div>
             </div>
             <div class="cart-total" v-if="uniqueCartItems.length > 0">
                 <h3>Total: {{ formatCurrency(cart.cartTotal) }}</h3>
@@ -68,5 +75,110 @@ onClickOutside(modalRef, () => {
 
 <style lang="scss" scoped>
 @use '../assets/style/variables' as v;
+
+.cart-modal.open {
+    .modal-container {
+        display: block;
+        padding: 0;
+    }
+}
+
+.cart-content {
+    padding: 20px;
+}
+
+.no-items {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    h1 {
+        font-size: v.$title-small;
+        color: v.$primary-color;
+    }
+}
+
+.cart-total {
+    padding: 20px;
+    background-color: v.$secondary-color;
+    h3 {
+        font-size: v.$subtitle-medium;
+        color: v.$white-color;
+    }
+}
+
+.cart-items {
+    overflow: auto;
+    width: 100%;
+    .cart-item {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 20px;
+        border-bottom: 1px solid v.$shadow-color;
+        .cart-wrap {
+            display: flex;
+            align-items: center;
+            .cart-item-img {
+                width: 50px;
+                height: 50px;
+                margin-right: 20px;
+                img {
+                    width: 100%;
+                    height: 100%;
+                    object-fit: cover;
+                }
+            }
+            .cart-item-info {
+                h3 {
+                    font-size: v.$subtitle-small;
+                    color: v.$secondary-color;
+                }
+                p {
+                    font-size: v.$text-small;
+                    color: v.$primary-color;
+                }
+            }
+        }
+        .cart-item-buttons {
+            display: flex;
+            align-items: center;
+            .cart-item-quantity {
+                display: flex;
+                align-items: center;
+                margin-right: 20px;
+                .btn-quantity {
+                    background-size: 20px;
+                    background-repeat: no-repeat;
+                    background-position: center;
+                    &.remove {
+                        background-image: url('../assets/img/less.png');
+                        background-size: 15px;
+                        &:hover {
+                            background-image: url('../assets/img/less-hover.png');
+                        }
+                    }
+                    &.add {
+                        background-image: url('../assets/img/plus.png');
+                        &:hover {
+                            background-image: url('../assets/img/plus-hover.png');
+                        }
+                    }
+                }
+                span {
+                    margin: 0 1rem;
+                    font-size: v.$text-small;
+                    color: v.$primary-color;
+                }
+            }
+            .btn-remove-all {
+                background-size: 15px;
+                background-repeat: no-repeat;
+                background-position: center;
+                background-image: url('../assets/img/trash.png');
+            }
+        }
+    }
+}
 
 </style>
--- src/stores/cart.js
+++ src/stores/cart.js
@@ -1,34 +1,35 @@
-import {defineStore} from 'pinia'
+import { defineStore } from 'pinia';
 import { ref, computed, watch } from 'vue';
 
 export const useCartStore = defineStore('cart', () => {
-    const cartValue = ref([])
-    let alert = ref(false)
-    let alertMsg = ref('Produto adicionado ao carrinho.')
+    const cartValue = ref([]);
+    let alert = ref(false);
+    let alertMsg = ref('Produto adicionado ao carrinho.');
+    
     const cartLength = computed(() => {
         return cartValue.value.length;
-    })
+    });
 
     const cartTotal = computed(() => {
         let total = 0;
         if (cartValue.value.length) {
             cartValue.value.forEach(product => {
-                total += product.price
-            })
+                total += product.price;
+            });
         }
-        return total
-    })
+        return total;
+    });
 
     function addProduct(product) {
         product.stock--;
-        const {id, name, price} = product;
-        cartValue.value.push({id, name, price});
+        const { id, name, price, img } = product;
+        cartValue.value.push({ id, name, price, img });
         alertMsg.value = `${name} adicionado ao carrinho.`;
         alert.value = true;
         
         setTimeout(() => {
             alert.value = false;
-        }, 2000)
+        }, 2000);
     }
 
     function removeProduct(index) {
